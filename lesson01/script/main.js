92387fefcdd8ca44e5ba01e88b6bea7aac7beb1f
evgeniyuk <53653637+evgeniyuk@users.noreply.github.com>
2019-09-28 16:39:23 +0300

    1
    DZ01

---
+++ lesson01/script/main.js
@@ -0,0 +1,10 @@
+
+console.log('hello')
+console.log('Задание 1. перевод значений температуры')
+
+var tempr_с = prompt('введите значение температуры по Цельсию:')
+var tempr_f = (9/5)*tempr_с+32
+
+ 
+
+console.log ('Температура по Фаренгейту: ' +  tempr_f)
